ce48f35caa3ba5cba7a7187932881e82c3d1998a
luis2 <luis212urbina@gmail.com>
2020-09-22 14:07:51 -0600

    Render lines done, fixes pending

--- src/screens/Information/index.js
+++ src/screens/Information/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-//import {View, Text} from 'react-native';
+import React, {Component} from 'react';
+// import {View, Text} from 'react-native';
 
 import {connect} from 'react-redux';
 import {getDestinationPath} from '../../redux/root-reducer';
@@ -26,41 +26,58 @@ import { deleteCurrentNode } from '../../redux/location/location.actions';
 import { isNavigationDone } from '../../redux/location/location.reducer';
 
 
-const InfoAR = ({path, del}) => {
+class Information extends React.Component {
+    constructor(props) {
+    super(props);
+        this.state = {
+            path: [],
+        };
+    }
+    
+    componentDidMount() {
+        console.log(this.props.path)
+        this.setState({path : this.props.path})
+    }
 
-    return (
-        <ViroARScene>
-            <ViroAmbientLight color="#FFFFFF" intensity={20} />
-            <ViroText
-                text="Sigue la línea!"
-                width={2}
-                height={2}
-                position={[0, 0, -3]}
-            />
-            <ViroButton
-                source={require("../../res/delete.png")}
-                position={[1, 3, -5]}
-                height={2}
-                width={3}
-                onTap={del}
-            />
-            {/* Render lines here */}
-            <ViroPolyline
-                position={[0, 0, -2]}
-                points={[
-                    [0, 0, 0],
-                    [0.5, 0.5, 0.5],
-                    [1, 0, 0],
-                ]}
-                thickness={0.2}
-            />
-        </ViroARScene>
-    );
-};
-
-const Information = ({del}) => {
-    return <ViroARSceneNavigator initialScene={{scene: InfoAR}} />;
-};
+    render() {
+        if (this.props.path !== null ){
+            // console.log(this.props.path.path)
+            const { path } = this.props.path;
+            console.log(path)
+            const arrayOfCoordinates = path.map(item => [item.node["coordinates"][1],item.node["coordinates"][2], item.node["coordinates"][0]])
+            console.log(arrayOfCoordinates)
+            const InfoAr = () => {
+                return (
+                    <ViroARScene>
+                        <ViroAmbientLight color="#FFFFFF" intensity={20} />
+                        <ViroText
+                            text="Sigue la línea!"
+                            width={2}
+                            height={2}
+                            position={[0, 0, -3]}
+                        />
+                        <ViroButton
+                            source={require("../../res/delete.png")}
+                            position={[1, 3, -5]}
+                            height={2}
+                            width={3}
+                            onTap={this.props.del}
+                        />
+                        {/* Render lines here */}
+                        <ViroPolyline
+                            position={[0, 0, -2]}
+                            points={arrayOfCoordinates}
+                            thickness={0.2}
+                        />
+                    </ViroARScene>
+                );
+            }
+            return <ViroARSceneNavigator  initialScene={{scene: InfoAr}} />;
+        } else {
+            return <><Text>Loading route</Text></>
+        }
+    }
+}
 
 const image = {uri: 'https://miro.medium.com/max/2400/0*VUGGU1mPbQG2QrFe.png'};
 
@@ -93,5 +110,5 @@ export default connect(
             dispatch(deleteCurrentNode())
         }
     }),
-)(deleteButton);
+)(Information);
 
